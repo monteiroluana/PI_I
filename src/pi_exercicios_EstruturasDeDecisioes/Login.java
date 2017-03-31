1338fd5a7581a537cc9fe8eff8ca738004d8f3b2
Luana Monteiro <luanamonteiropereira95@gmail.com>
2017-03-30 22:20:52 -0300

    30/03/17

--- src/pi_exercicios_EstruturasDeDecisioes/Login.java
+++ src/pi_exercicios_EstruturasDeDecisioes/Login.java
@@ -19,13 +19,22 @@ public class Login {
         String Login = JOptionPane.showInputDialog("Login");
         String Senha = JOptionPane.showInputDialog("Senha");
 
-        /*Processamento*/
+        /*Processamento
         if (Login.equals("guilherme") && Senha.equals("0f5")) {
             JOptionPane.showMessageDialog(null, "Bem vindo, " + Login);
         } else if (Login.equals("jair") && Senha.equals("0706")) {
             JOptionPane.showMessageDialog(null, "Bem vindo, " + Login);
         } else {
             JOptionPane.showMessageDialog(null, "Login ou Senha Inválido");
+        }*/
+
+        /*Forma do Prof*/
+        boolean autorizado = (Login.equals("guilherme") && Senha.equals("0f5")) || (Login.equals("jair") && Senha.equals("0706"));
+        if (autorizado) {
+            JOptionPane.showMessageDialog(null, "Bem vindo, " + Login);
+        } else {
+            JOptionPane.showMessageDialog(null, "Login ou Senha Inválido");
         }
+
     }
 }
