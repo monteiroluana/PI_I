a7115813aa24ea11914978ea09dda3e1c6c8b4e9
Luana Monteiro <luanamonteiropereira95@gmail.com>
2017-04-28 00:27:32 -0300

    JogoDaVelha (Funcionando)

--- src/Aula/JogoDaVelha.java
+++ src/Aula/JogoDaVelha.java
@@ -11,6 +11,9 @@ public class JogoDaVelha {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        String p1 = "", p2 = "", p3 = "", p4 = "", p5 = "", p6 = "", p7 = "", p8 = "", p9 = "";
+        String vez = "", posicao, pAnterior = "0";
+        int i = 0;
 
         /*1 Monstrando tabuleiro*/
         String mosTabuleiro = " 1 | 2 | 3 \n";
@@ -20,83 +23,106 @@ public class JogoDaVelha {
         mosTabuleiro += " 7 | 8 | 9 \n";
         System.out.println(mosTabuleiro);
 
-        /*Posições*/
-        String p1 = "", p2 = "", p3 = "", p4 = "", p5 = "", p6 = "", p7 = "", p8 = "", p9 = "";
-        String vez = "", posicao, pAnterior = "";
+        do {
 
-        for (int i = 0; i < 9; i++) {
-            /*Pedindo e vez e Validando 
-            System.out.print("De quem é a vez X ou O : ");
-            vez = console.next();
-            if (vez.equals(vezAnterior)) {
-            }*/
-
-
- /*DE QUEM È A VEZ*/
-            vez = vez.toUpperCase();
-            if (vez.equals("X")) {
-                vez = "O";
-            } else {
-                vez = "X";
-            }
-            System.out.println("Vez do: " + vez);
-
-            /*Pedindo a Posição da jogada*/
+            /*POSIÇÃO DA JOGADA*/
             System.out.print("Posição: ");
             posicao = console.next();
 
-             if (pAnterior.contains(posicao)) {
+            /*Verificando se a posição é igual a 1,2,3,4,5,6,7,8,9 */
+            if (!pAnterior.contains(posicao)) {
+                if (posicao.equals("1") || posicao.equals("2") || posicao.equals("3") || posicao.equals("4") || posicao.equals("5") || posicao.equals("6") || posicao.equals("7") || posicao.equals("8") || posicao.equals("9")) {
+                    pAnterior += posicao;
+                    i++;
 
-                System.out.println("Contem");
+                    /*DE QUEM É A VEZ*/
+                    vez = vez.toUpperCase();
+                    if (vez.equals("X")) {
+                        vez = "O";
+                    } else {
+                        vez = "X";
+                    }
+                    System.out.println("Vez do: " + vez);
+
+                    switch (posicao) {
+
+                        case "1":
+                            p1 = vez;
+                            break;
+                        case "2":
+                            p2 = vez;
+                            break;
+                        case "3":
+                            p3 = vez;
+                            break;
+                        case "4":
+                            p4 = vez;
+                            break;
+                        case "5":
+                            p5 = vez;
+                            break;
+                        case "6":
+                            p6 = vez;
+                            break;
+                        case "7":
+                            p7 = vez;
+                            break;
+                        case "8":
+                            p8 = vez;
+                            break;
+                        case "9":
+                            p9 = vez;
+                            break;
+
+                    }
+
+                    /*MOSTRANDO A JOGADA*/
+                    String Tabuleiro = "";
+                    Tabuleiro += " " + p1 + " | " + p2 + " | " + p3 + " \n";
+                    Tabuleiro += "---|---|---\n";
+                    Tabuleiro += " " + p4 + " | " + p5 + " | " + p6 + " \n";
+                    Tabuleiro += "---|---|---\n";
+                    Tabuleiro += " " + p7 + " | " + p8 + " | " + p9 + " \n";
+                    System.out.println(Tabuleiro);
+
+                    /*VERIFICANDO SE TEM UM VENCEDOR*/
+                    if (!p1.isEmpty() && p1.equals(p2) && p1.equals(p3)) {
+                        System.out.println(p1 + " VENCEU!");
+                        break;
+                    } else if (!p2.isEmpty() && p2.equals(p5) && p5.equals(p8)) {
+                        System.out.println(p2 + " VENCEU!");
+                        break;
+                    } else if (!p3.isEmpty() && p3.equals(p6) && p6.equals(p9)) {
+                        System.out.println(p3 + " VENCEU!");
+                        break;
+
+                    } else if (!p1.isEmpty() && p1.equals(p2) && p2.equals(p3)) {
+                        System.out.println(p1 + " VENCEU!");
+                        break;
+                    } else if (!p4.isEmpty() && p4.equals(p5) && p5.equals(p6)) {
+                        System.out.println(p4 + " VENCEU!");
+                        break;
+                    } else if (!p7.isEmpty() && p7.equals(p8) && p8.equals(p9)) {
+                        System.out.println(p7 + " VENCEU!");
+                        break;
+
+                    } else if (!p1.isEmpty() && p1.equals(p5) && p5.equals(p9)) {
+                        System.out.println(p1 + " VENCEU!");
+                        break;
+                    } else if (!p3.isEmpty() && p3.equals(p5) && p5.equals(p7)) {
+                        System.out.println(p3 + " VENCEU!");
+                        break;
+                    }
+
+                    /*POSIÇÕES INVÁLIDAS*/
+                } else {
+                    System.out.println("Posição inválida");
+                }
             } else {
-                System.out.println("não contem");
+                System.out.println("Posição inválida");
             }
-            
-            
-            
 
-            switch (posicao) {
+        } while (i <= 9);
 
-                case "1":
-                    p1 = vez;
-                    break;
-                case "2":
-                    p2 = vez;
-                    break;
-                case "3":
-                    p3 = vez;
-                    break;
-                case "4":
-                    p4 = vez;
-                    break;
-                case "5":
-                    p5 = vez;
-                    break;
-                case "6":
-                    p6 = vez;
-                    break;
-                case "7":
-                    p7 = vez;
-                    break;
-                case "8":
-                    p8 = vez;
-                    break;
-                case "9":
-                    p9 = vez;
-                    break;
-
-            }
-            String Tabuleiro = " " + p1 + " | " + p2 + " | " + p3 + " \n";
-            Tabuleiro += "---|---|---\n";
-            Tabuleiro += " " + p4 + " | " + p5 + " | " + p6 + " \n";
-            Tabuleiro += "---|---|---\n";
-            Tabuleiro += " " + p7 + " | " + p8 + " | " + p9 + " \n";
-            System.out.println(Tabuleiro);
-
-            pAnterior += posicao;
-            System.out.println(pAnterior);
-            
-           
-        }
     }
 }
